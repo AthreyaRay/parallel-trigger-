d82bf5f0c07efb3488184f25ac1c258cc4c617d1
Athreya Rayaprolu <athreya@Athreyas-MacBook-Pro.local>
2024-03-14 14:38:15 -0400

    adding test script and testing step wise notification

---
+++ test-script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+step_result=$(buildkite-agent step get "outcome" --step "build")
+
+if [ "$step_result" = "passed" ]; then
+    cat <<YAML
+steps:
+  - command: "echo 'Step has passed'"
+    label: ":white_check_mark: Step Passed"
+    notify:
+      email: "athreya@buildkite.com"
+YAML
+else
+    cat <<YAML
+steps:
+  - command: "echo 'It failed'"
+    label: ":x: Step Failed"
+    notify:
+      email: "athreya@buildkite.com"
+YAML
+fi
+
